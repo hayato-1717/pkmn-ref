0c5a6b17255030f9ed4378222970c479008ecda7
hayato-1717 <hayato-1717@users.noreply.github.com>
2026-09-06 09:53:29 +0900

    M-5実採用率で環境55型を更新（ブリジュラス物理受け型/ミミッキュ珠1.3/カイリューしんそく/マフォクシーマジカルシャイン等）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 自動生成（build.py）。手で編集しない。
-const V = "pkmn-1ca0fc8baa98";
+const V = "pkmn-60a50104204a";
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(ks =>
